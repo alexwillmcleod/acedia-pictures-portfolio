58ff84678a9e88d1cc7d012a33579565a7f79635
Alex McLeod <alexwillmcleod@gmail.com>
2023-01-21 23:10:54 +1300

    Switched vercel to deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
+
+// https://astro.build/config
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: deno(),
 });
