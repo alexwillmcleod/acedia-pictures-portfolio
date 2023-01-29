9ab0b0ecdfc5eb98fba498681181b542e6ca7be2
Alex McLeod <alexwillmcleod@gmail.com>
2023-01-30 12:09:56 +1300

    Added feature page

---
+++ src/components/featured/component.tsx
@@ -0,0 +1,60 @@
+import styles from './style.module.css';
+import featuredProjects from './featured.conf.json';
+
+type TagProps = {
+  text: string;
+  color: string;
+};
+
+export const Tag = (props: TagProps) => {
+  return (
+    <p
+      className={styles.tag}
+      style={{ color: props.color }}
+    >
+      {props.text}
+    </p>
+  );
+};
+
+type FeaturedProject = {
+  title: string;
+  description?: string;
+  coverImage: string;
+  videoRef: string;
+  tags?: TagProps[];
+};
+
+export const FeaturedComponent = (props: FeaturedProject) => {
+  return (
+    <div
+      className={styles['featured-component-container']}
+      style={{
+        backgroundImage: `url("${props.coverImage}")`,
+      }}
+    >
+      <div className={styles['featured-component-container-child']}>
+        <h1 className={styles['featured-component-title']}>
+          {props.title}
+          {props.tags ? (
+            props.tags.map((element) => <Tag {...element} />)
+          ) : (
+            <></>
+          )}
+        </h1>
+        <p className={styles.description}>{props.description}</p>
+        <a href={props.videoRef}></a>
+      </div>
+    </div>
+  );
+};
+
+export const Featured = () => {
+  return (
+    <div className={styles.container}>
+      {featuredProjects.map((element) => (
+        <FeaturedComponent {...element} />
+      ))}
+    </div>
+  );
+};
