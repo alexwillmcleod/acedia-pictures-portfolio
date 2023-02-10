9f45b35e4ae580c396f096529278ef32e4e82753
Alexander McLeod <alexwillmcleod@gmail.com>
2023-02-10 16:23:35 +1300

    Filled featured page with content

--- src/components/featured/component.tsx
+++ src/components/featured/component.tsx
@@ -22,7 +22,7 @@ type FeaturedProject = {
   key: string;
   description?: string;
   coverImage: string;
-  videoRef: string;
+  videoRef?: string;
   tags?: TagProps[];
 };
 
@@ -45,7 +45,23 @@ export const FeaturedComponent = (props: FeaturedProject) => {
           )}
         </h1>
         <p className={styles.description}>{props.description}</p>
-        <a href={props.videoRef}></a>
+        {props.videoRef && (
+          <a
+            className={styles['video-link']}
+            href={props.videoRef}
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              viewBox="0 0 24 24"
+              fill="white"
+            >
+              <path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z" />
+            </svg>
+          </a>
+        )}
       </div>
     </div>
   );
